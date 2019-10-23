6cbbd15cb4fd8359a72c91b8fa034c2054e4daf3
David Pérez Gallego <davidperezgallego@gmail.com>
2019-10-23 18:12:48 +0200

    Added normal calculation

--- RaytracingOneWeekend/main.cpp
+++ RaytracingOneWeekend/main.cpp
@@ -6,22 +6,29 @@
 
 #include "ray.h"
 
-bool hit_sphere(const glm::vec3& center, float radius, const ray& r) {
+float hit_sphere(const glm::vec3& center, float radius, const ray& r) {
 	glm::vec3 O_C = r.origin() - center;
 	float a = glm::dot(r.direction(), r.direction());
-	float b = 2.0 * glm::dot(O_C, r.direction());
+	float b = 2.0f * glm::dot(O_C, r.direction());
 	float c = glm::dot(O_C, O_C) - radius * radius;
 	float discriminant = b * b - 4.0f * a * c;
-	return (discriminant > 0);
+	
+	if (discriminant < 0) {
+		return -1.0f;
+	}
+	return ((-b - std::sqrt(discriminant)) / (2.0f * a));
 }
 
 glm::vec3 color(const ray& r) {
-	if (hit_sphere(glm::vec3(0,0,-1),0.5, r)) {
-		return glm::vec3(1.f, 0.f, 0.f);
+	float t = hit_sphere(glm::vec3(0, 0, -1), 0.5, r);
+	if (t > 0.0f) {
+		glm::vec3 spaceRelativeToSphere = r.point_at_parameter(t) - glm::vec3(0, 0, -1);
+		glm::vec3 normal = glm::normalize(r.point_at_parameter(t) - glm::vec3(0, 0, -1));
+		return 0.5f * glm::vec3(normal.x + 1, normal.y + 1, normal.z + 1);
 	}
 
 	glm::vec3 normDir = glm::normalize(r.direction());
-	float t = 0.5 * (normDir.y + 1.0); //[0,1]
+	t = 0.5f * (normDir.y + 1.0f); //[0,1]
 	return (1.0f - t) * glm::vec3(1.0, 1.0, 1.0) + t * glm::vec3(0.5, 0.7, 1.0); //higher ray direction -> less gray scale and more bluish color. LERP
 }
 
@@ -56,7 +63,12 @@ int main() {
 	}
 
 	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+#if _DEBUG
 	std::chrono::milliseconds time_span = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-	std::cout << "I took " << time_span.count() << std::endl;
+	std::cout << "I took " << time_span.count() << " milliseconds" <<std::endl;
+#else
+	std::chrono::nanoseconds time_span = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1);
+	std::cout << "I took " << time_span.count() <<" nanoseconds" <<std::endl;
+#endif
 	file.close();
 }
